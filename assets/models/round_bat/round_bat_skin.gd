ca61b188d4d2b59063a1831ef8bee09cd398a340
Harits <mharitsnf@gmail.com>
2023-08-05 10:24:19 +0200

    custom progress bar

---
+++ assets/models/round_bat/round_bat_skin.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@onready var _animation_tree: AnimationTree = %AnimationTree
+@onready var _hurt_shot_path: String = "parameters/HurtShot/request"
+
+## Play a OneShot hurt animation.
+## This animation is played in paralel with other states.
+func hurt():
+	_animation_tree.set(_hurt_shot_path, true)
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept"):
+		hurt()
